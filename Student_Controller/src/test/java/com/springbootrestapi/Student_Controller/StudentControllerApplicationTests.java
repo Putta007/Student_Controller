aea2936823f55879bd72430091d6fe2824dc42a8
Putta007 <131178606+Putta007@users.noreply.github.com>
2023-04-18 11:01:46 -0400

    Add files via upload

---
+++ Student_Controller/src/test/java/com/springbootrestapi/Student_Controller/StudentControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootrestapi.Student_Controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
